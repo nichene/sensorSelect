8c71c1d0ff48e3f5fe130e0479a1330b279e1961
nichene <nichene@users.noreply.github.com>
2017-02-13 20:32:59 -0300

    Add files via upload

--- sensorModel.py
+++ sensorModel.py
@@ -1,20 +1,17 @@
 from gurobipy import *
 import traceback
 
-n = 4 #sensores(i) por tipo
-m = 5 #numero de tipos(j)
-B = 9 #Budget do usuário
+n = 4 #Sensores(i) por tipo
+m = 5 #Numero de tipos(j)
+B = 9 #Orçamento do usuário
 
 
 try:
-	#CREATE MODEL
+	#Criação do modelo
 	model = Model("SensorSelect")
 
-	#CREATE VARIABLES
-
-	#matriz sensor i tipo j. 5 tipos, cada tipo tem 4 sensores.
-	#matching é indice de 0-1.
-	# M é o matching dos requisitos.  
+	#Matriz sensor i tipo j. 5 tipos, cada tipo tem 4 sensores.
+	#M = atendimento dos requisitos, variando entre 0-1.  
 	M = [[0.2, 0.3, 0.4, 0, 0],
 	    [0, 0, 0.5, 0, 0],
 	    [0, 0, 0.6, 0, 0],
@@ -23,11 +20,12 @@ try:
 	nrange = range(n)
 	mrange = range(m)
 
-	#DECISION VARIABLE: X[i,j] == 1 if sensor is selected
+	#Criação das variáveis de decisão: 
+	#X[i,j] == 1 se sensor for selecionado; 0 caso contrário
 	X = model.addVars(nrange, mrange, vtype=GRB.BINARY, name="X")
 	#print(X[(0,0)])
 
-	#SET OBJECTIVE
+	#Estruturação da função objetivo
 	listaProdutosMX = []
 	for i in nrange:
 		for j in mrange:
@@ -35,9 +33,8 @@ try:
 
 	model.setObjective(sum(listaProdutosMX),GRB.MAXIMIZE)
 
-	#ADD CONSTRAINTS
-
-	# Value. valores dos sensores i do tipo j. 
+	#Definição dos custos dos sensores.
+	# V = custos dos sensores i do tipo j. 
 	V = [[20, 10, 1, 2, 3],
 	     [ 2,  3, 5, 4,30],
 	     [ 6,  8, 8, 9,10],
@@ -50,15 +47,16 @@ try:
 		for j in mrange:
 			listaProdutosVX.append(V[i][j] * X[(i,j)])
 
+	#Definição das restrições
 	model.addConstr(sum(listaProdutosVX), GRB.LESS_EQUAL, B, "c0")
 	
 	for j in mrange:
 		model.addConstr(X.sum('*',j), GRB.GREATER_EQUAL, N[j], "c1")
 
-	#OPTIMIZE
+	#Otimização
 	model.optimize()
 	
-	#PRINT DECISION VARIABLES
+	#resposta das variáveis de decisão.
 	if (model.solCount > 0 ): 
 		for v in model.getVars():
 			print(v.varName, v.x)
